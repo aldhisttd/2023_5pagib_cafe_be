07b57de315c3976348d391e140d45a0c350c3a6d
sitijamila32 <ilaee32@gmail.com>
2023-12-30 16:02:43 +0700

    revi api update menu be

--- obat_update.php
+++ obat_update.php
@@ -5,51 +5,35 @@ $response = [
     'status' => '',
     'msg' => '',
     'body' => [
-        'data' => [
-            'kode' => '',
-            'nama' => '',
-            'kode_kategori' => '',
-            'gambar' => '',
-            'harga' => ''
-        ]
+        'data' => []
     ]
 ];
 
-// Cek koneksi ke database
-$koneksi = mysqli_connect('localhost', 'root', '', 'cafe');
-if (!$koneksi) {
-    $response['status'] = ''; // Sesuaikan dengan nilai yang diinginkan
-    $response['msg'] = 'Gagal terhubung ke database';
-} else {
-    $kode = isset($_POST['kode']) ? mysqli_real_escape_string($koneksi, $_POST['kode']) : '';
-    $nama = isset($_POST['nama']) ? mysqli_real_escape_string($koneksi, $_POST['nama']) : '';
-    $kode_kategori = isset($_POST['kode_kategori']) ? mysqli_real_escape_string($koneksi, $_POST['kode_kategori']) : '';
-    $harga = isset($_POST['harga']) ? mysqli_real_escape_string($koneksi, $_POST['harga']) : '';
+// Periksa apakah kunci 'kode', 'nama', 'kode_kategori', dan 'harga' ada dalam $_POST
+if (isset($_POST['kode'], $_POST['nama'], $_POST['kode_kategori'], $_POST['harga'])) {
+    $kode = $_POST['kode'];
+    $nama = $_POST['nama'];
+    $kode_kategori = $_POST['kode_kategori'];
+    $harga = $_POST['harga'];
 
-    // Periksa apakah ada file gambar yang diupload
     if (isset($_FILES['gambar']) && $_FILES['gambar']['name'] != "") {
-
         // ambil nama gambar lama
         $q = mysqli_query($koneksi, "SELECT gambar FROM menu WHERE kode='$kode'");
         $dt = mysqli_fetch_array($q);
         $gambar = isset($dt['gambar']) ? $dt['gambar'] : '';
 
-        // hapus gambar lama
-        if (!empty($gambar) && file_exists('uploads/' . $gambar)) {
-            unlink('uploads/' . $gambar);
-        }
+        unlink('upload/'.$gambar);
 
-        // upload gambar baru
         $path = $_FILES['gambar']['name'];
         $ext = "." . pathinfo($path, PATHINFO_EXTENSION);
         $namagambar = md5(time()) . $ext;
-        move_uploaded_file($_FILES['gambar']['tmp_name'], 'uploads/' . $namagambar);
+        move_uploaded_file($_FILES['gambar']['tmp_name'], 'upload/' . $namagambar);
 
         // update nama gambar ke gambar baru
         mysqli_query($koneksi, "UPDATE menu SET gambar='$namagambar' WHERE kode='$kode'");
     }
 
-    // update dengan condition
+    // update dengan kondisi
     $query = mysqli_query($koneksi, "UPDATE menu SET nama='$nama', kode_kategori='$kode_kategori', harga='$harga' WHERE kode='$kode'");
 
     if ($query) {
@@ -64,9 +48,6 @@ if (!$koneksi) {
         $response['status'] = 400;
         $response['msg'] = 'Data Gagal Diperbarui';
     }
-
-    // Tutup koneksi database
-    mysqli_close($koneksi);
 }
 
 echo json_encode($response);
