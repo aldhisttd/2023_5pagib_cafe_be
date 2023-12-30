5f5f60df4f96413ba63b532fd133d6ea0b1ec7c9
Suhendri12Ok <suhendri141002@gmail.com>
2023-12-30 15:53:51 +0700

    revisi api data menuu be

--- obat_read.php
+++ obat_read.php
@@ -9,36 +9,18 @@ $response = [
     ]
 ];
 
-// Coba terhubung ke database
-$koneksi = mysqli_connect('localhost', 'root', '', 'cafe');
+$q = mysqli_query($koneksi, "SELECT * FROM menu");
 
-if ($koneksi) {
-    // Lakukan query untuk mendapatkan data
-    $q = mysqli_query($koneksi, "SELECT * FROM menu");
+$dataArray = mysqli_fetch_all($q, MYSQLI_ASSOC);
 
-    // Mengambil semua baris hasil query sebagai array
-    $dataArray = mysqli_fetch_all($q, MYSQLI_ASSOC);
-
-    // Memeriksa apakah ada data yang ditemukan
-    if (!empty($dataArray)) {
-        $response['status'] = 200;
-        $response['message'] = 'Success';
-        $response['body']['data'] = $dataArray;
-    } else {
-        $response['status'] = 400;
-        $response['message'] = 'Data tidak ditemukan';
-    }
-
-    // Tutup koneksi database
-    mysqli_close($koneksi);
+if (!empty($dataArray)) {
+    $response['status'] = 200;
+    $response['message'] = 'Success';
+    $response['body']['data'] = $dataArray;
 } else {
-    $response['status'] = 500;
-    $response['message'] = 'Gagal terhubung ke database';
+    $response['status'] = 400;
+    $response['message'] = 'error';
 }
 
-// Set header untuk JSON
-header('Content-Type: application/json');
-
-// Mengirimkan respons JSON ke client
 echo json_encode($response);
 ?>
