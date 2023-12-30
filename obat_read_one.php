d689b5e52c5080d5563b64ca91877653879c45cd
D-lemillion <dyanafrnd@gmail.com>
2023-12-30 11:00:43 +0700

    revisi api data by id menu

--- obat_read_one.php
+++ obat_read_one.php
@@ -2,39 +2,30 @@
 include 'env.php';
 
 $response = [
+    'status' => '',
+    'msg' => '',
     'body' => [
-        'data' => [
-            'kode' => '',
-            'nama' => '',
-            'kode_kategori' => '',
-            'gambar' => '',
-            'harga' => ''
-        ]
+        'data' => []
     ]
 ];
 
-$koneksi = mysqli_connect('localhost', 'root', '', 'cafe');
-if ($koneksi) {
-    $kode = isset($_GET['kode']) ? mysqli_real_escape_string($koneksi, $_GET['kode']) : '';
+$kode = isset($_GET['kode']) ? mysqli_real_escape_string($koneksi, $_GET['kode']) : '';
 
-    if (!empty($kode)) {
-        $query = mysqli_query($koneksi, "SELECT * FROM menu WHERE kode='$kode'");
+if (!empty($kode)) {
+    $query = mysqli_query($koneksi, "SELECT * FROM menu WHERE kode='$kode'");
 
-        if ($query) {
-            $menuData = mysqli_fetch_assoc($query);
+    if ($query) {
+        $menuData = mysqli_fetch_assoc($query);
 
-            if ($menuData) {
-                $response['status'] = 200;
-                $response['msg'] = 'Succes';
-                $response['body']['data'] = $menuData;
-            } else {
-                $response['status'] = 400;
-                $response['msg'] = 'Error';
-            }
+        if ($menuData) {
+            $response['status'] = 200;
+            $response['msg'] = 'Success';
+            $response['body']['data'] = $menuData;
+        } else {
+            $response['status'] = 400;
+            $response['msg'] = 'Error';
         }
     }
-
-    mysqli_close($koneksi);
 }
 
 echo json_encode($response);
