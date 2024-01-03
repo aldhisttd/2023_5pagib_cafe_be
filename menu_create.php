2538bba191c0f7d6b32009d17e2364b5a312f888
sitiannisaa <sitiannisamaulidya21@gmail.com>
2024-01-03 22:50:37 +0700

    33_API_input_menu

---
+++ menu_create.php
@@ -0,0 +1,44 @@
+<?php
+include 'env.php';
+
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => []
+    ]
+];
+
+$kode = $_POST['kode'];
+$nama = $_POST['nama'];
+$kode_kategori = $_POST['kode_kategori'];
+$harga = $_POST['harga'];
+
+// Proses upload gambar
+if (isset($_FILES['gambar'])) {
+    $uploadDir = 'upload/';
+    $uploadFile = $uploadDir . basename($_FILES['gambar']['name']);
+    $gambar = $_FILES['gambar']['name'];
+
+    if (move_uploaded_file($_FILES['gambar']['tmp_name'], $uploadFile)) {
+        // Jika upload gambar berhasil, lanjutkan proses penyimpanan data ke database
+        $query = mysqli_query($koneksi, "INSERT INTO menu (kode, nama, kode_kategori, gambar, harga) 
+                                          VALUES ('$kode', '$nama', '$kode_kategori', '$gambar', '$harga')");
+
+        if ($query) {
+            $response['status'] = 200;
+            $response['msg'] = 'Data berhasil diinsert';
+            $response['body']['data']['kode'] = $kode;
+            $response['body']['data']['nama'] = $nama;
+            $response['body']['data']['kode_kategori'] = $kode_kategori;
+            $response['body']['data']['gambar'] = $gambar;
+            $response['body']['data']['harga'] = $harga;
+        } else {
+            $response['status'] = 400;
+            $response['msg'] = 'Gagal membuat menu: ' . mysqli_error($koneksi);
+        }
+    }
+}
+
+echo json_encode($response);
+?>
