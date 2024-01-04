cbc620df67e4255839f1b8cd05833d802c97b56d
FanyFadhilah <fdhlhfny@gmail.com>
2024-01-05 03:09:05 +0700

    31_API_delete_kategori

--- kategori_delete.php
+++ kategori_delete.php
@@ -8,24 +8,19 @@ $response = [
         'data' => []
     ]
 ];
-if (isset($_POST['kode'])) {
-    $kode = $_POST['kode'];
-    if (!empty($kode)) {
-        $selectQuery = mysqli_query($koneksi, "SELECT * FROM menu WHERE kode='$kode'");
-        $deletedData = mysqli_fetch_assoc($selectQuery);
-        $query = mysqli_query($koneksi, "DELETE FROM kategori WHERE kode='$kode'");
 
-        if ($query) {
-            $response['status'] = 200;
-            $response['msg'] = 'Data berhasil dihapus';
-            $response['body']['data']['kode'] = $kode;  
-        } else {
-            $response['status'] = 400;
-            $response['msg'] = 'Data gagal dihapus';
-            $response['body']['data']['kode'] = $kode;  
-        }
+$kode = $_POST['kode'];
+
+$query = mysqli_query($koneksi, "DELETE FROM kategori WHERE kode='$kode'");
+
+    if ($query) {
+        $response['status'] = 200;
+        $response['msg'] = 'Data berhasil dihapus';
+        $response['body']['data']['kode'] = $kode;
+    } else {
+        $response['status'] = 400;
+        $response['msg'] = 'Data gagal dihapus';
+        $response['body']['data']['kode'] = $kode;
     }
-}
-
 echo json_encode($response);
 ?>
