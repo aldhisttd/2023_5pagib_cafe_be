6ec5622ed848de15e62e6464f756691683ea45e2
sitijamila32 <ilaee32@gmail.com>
2024-01-05 02:28:16 +0700

    37_API_Update_Menu

--- menu_update.php
+++ menu_update.php
@@ -16,27 +16,20 @@ $harga = $_POST['harga'];
 
 if ($_FILES["gambar"]["name"] != "") {
     // ambil nama gambar lama
-    $q = mysqli_query($koneksi, "SELECT gambar FROM menu WHERE kode='$kode'");
-    $dt = mysqli_fetch_array($q);
-    $gambar = $dt['gambar'];
+    $query = mysqli_query($koneksi, "SELECT gambar FROM menu WHERE kode = '$kode'");
+    $data = mysqli_fetch_assoc($query);
+    $gambar = $data['gambar'];
 
     unlink($gambar);
 
     $temp = explode(".", $_FILES["gambar"]["name"]);
-    $namagambar = md5(date('dmy h:i:s')) . '.' . end($temp);
+    $gambarbaru = md5(date('dmy h:i:s')) . '.' . end($temp);
+    $target_file = "upload/" . $gambarbaru;
+    move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
 
-    $uploadDir = 'upload/';
-    if (!file_exists($uploadDir)) {
-        mkdir($uploadDir, 0777, true);
-    }
-
-    $target_file = $uploadDir . $namagambar;
-    if (move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file)) {
-        mysqli_query($koneksi, "UPDATE menu SET gambar = '$target_file' WHERE kode = '$kode'");
-    }
+    mysqli_query($koneksi, "UPDATE menu SET gambar = 'upload/$gambarbaru' WHERE kode = '$kode'");
 }
 
-// Update data dengan kondisi
 $query = mysqli_query($koneksi, "UPDATE menu SET nama='$nama', kode_kategori='$kode_kategori', harga='$harga' WHERE kode='$kode'");
 
 if ($query) {
@@ -45,7 +38,7 @@ if ($query) {
     $response['body']['data']['kode'] = $kode;
     $response['body']['data']['nama'] = $nama;
     $response['body']['data']['kode_kategori'] = $kode_kategori;
-    $response['body']['data']['gambar'] = $target_file;
+    $response['body']['data']['gambar'] = 'upload/' . $gambarbaru;
     $response['body']['data']['harga'] = $harga;
 } else {
     $response['status'] = 400;
