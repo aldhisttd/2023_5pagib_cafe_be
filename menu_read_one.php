3685b1f1c32f6689aea933ab2df007e5204ec06c
D-lemillion <dyanafrnd@gmail.com>
2024-01-05 04:49:46 +0700

    35_API_data_by_id_menu

---
+++ menu_read_one.php
@@ -0,0 +1,41 @@
+<?php
+include 'env.php';
+
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+    'data' => []
+]
+];
+
+$kode = $_REQUEST['kode'];
+
+$query = mysqli_query($koneksi, "SELECT * FROM menu WHERE kode='$kode'");
+
+    if ($query) {
+        $menuData = mysqli_fetch_assoc($query);
+
+        if ($menuData) {
+            $response = [
+                'status' => 200,
+                'msg' => 'success',
+                'body' => [
+                    'data' => [
+                        'kode' => $$menuData['kode'],
+                        'nama' => $$menuData['nama'],
+                        'kode_kategori' => $$menuData['kode_kategori'],
+                        'gambar' => $$menuData['gambar'],
+                        'harga' => $$menuData['harga']
+                    ]
+                ]
+            ]
+        } else {
+            $response['status'] = 400;
+            $response['msg'] = 'Error';
+        }
+    }
+
+
+echo json_encode($response);
+?>
