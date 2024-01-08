01c4a5618cc65e5edca7858efc289a6b2594c0bc
rizkiiadriian <rizkii.adriian@gmail.com>
2024-01-08 19:47:16 +0700

    36. api delete menu be

---
+++ menu_delete.php
@@ -0,0 +1,33 @@
+<?php
+include 'env.php';
+
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => []
+    ]
+];
+
+$kode = $_POST['kode'];
+
+$query = mysqli_query($koneksi, "SELECT gambar FROM menu WHERE kode='$kode'");
+$data = mysqli_fetch_assoc($query);
+$gambar = $data['gambar'];
+unlink($gambar);
+
+$data = mysqli_query($koneksi, "DELETE FROM menu WHERE kode = '$kode'");
+
+if ($data) {
+
+    $response['status'] = 200;
+    $response['msg'] = 'Data berhasil dihapus';
+    $response['body']['data']['kode'] = $kode;
+} else {
+
+    $response['status'] = 400;
+    $response['msg'] = 'Data gagal dihapus';
+    $response['body']['data']['kode'] = $kode;
+}
+
+echo json_encode($response);
