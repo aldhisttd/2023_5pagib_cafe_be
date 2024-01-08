fa00fe74a05cfebb3f44cdc22b38be8b93365f11
rizkiiiiiii <129470362+rizkiiadriian@users.noreply.github.com>
2024-01-08 13:29:12 +0700

    revisi kategori_read_one.php

--- kategori_read_one.php
+++ kategori_read_one.php
@@ -5,24 +5,44 @@ $response = [
     'status' => '',
     'msg' => '',
     'body' => [
-        'data' => []
+        'data' => [
+            
+        ]  
     ]
 ];
 
-$kode = $_GET['kode'];
+$kode = $_REQUEST['kode'];
 
-if(!isset($koneksi)){
+$query = mysqli_prepare($koneksi, "SELECT menu.*, kategori.nama
+                                   FROM menu
+                                   LEFT JOIN kategori ON menu.kode_kategori = kategori.kode
+                                   WHERE menu.kode_kategori = ?");
 
+mysqli_stmt_bind_param($query, "s", $kode);
+mysqli_stmt_execute($query);
+
+$result = mysqli_stmt_get_result($query);
+$menuData = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+if ($menuData) {
+    $response = [
+        'status' => 200,
+        'msg' => 'success',
+        'body' => [
+            'data' => [
+                'kode' => $menuData[0]['kode'],
+                'nama' => $menuData[0]['nama'],
+                'kode_kategori' => $menuData[0]['kode_kategori'],
+                'nama_kategori' => $menuData[0]['nama'],
+                'gambar' => $menuData[0]['gambar'],
+                'harga' => $menuData[0]['harga'],
+            ]
+        ]
+    ];
+} else {
     $response['status'] = 400;
-    $response['msg'] = 'error';
-}else{
-    
-    $result = mysqli_query($koneksi, "SELECT * FROM kategori WHERE kode = '$kode'");
-    $row = mysqli_fetch_assoc($result);
-
-    $response['status'] = 200;
-    $response['msg'] = 'success';
-    $response['body']['data'] = $row;
+    $response['msg'] = 'Error';
 }
 
 echo json_encode($response);
+?>
