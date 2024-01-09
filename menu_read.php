b31a6e36a2de5666b07cbbdd792cf0af07dc7f11
Suhendri12Ok <129470519+Suhendri12Ok@users.noreply.github.com>
2024-01-09 19:03:18 +0700

    Update menu_read.php

--- menu_read.php
+++ menu_read.php
@@ -9,9 +9,11 @@ $response = [
     ]
 ];
 
-$q = mysqli_query($koneksi, "SELECT * FROM menu 
-                INNER JOIN kategori ON  menu.kode_kategori = kategori.kode");
+$query = "SELECT A.kode, A.nama, A.kode_kategori, B.nama as nama_kategori, A.gambar, A.harga
+          FROM menu A
+          INNER JOIN kategori B ON A.kode_kategori = B.kode";
 
+$q = mysqli_query($koneksi, $query);
 $dataArray = mysqli_fetch_all($q, MYSQLI_ASSOC);
 
 if ($dataArray){
